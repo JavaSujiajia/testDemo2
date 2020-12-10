826d64afc181e3fe635ae39c43fb1f796844d068
sujia <18231010309@163.com>
2020-12-10 17:09:09 +0800

    第一次上传

---
+++ src/main/java/com/example/demo/receiver/DirectReceiver.java
@@ -0,0 +1,25 @@
+package com.example.demo.receiver;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 文件描述
+ *
+ * @author sujia
+ * @date 2020年12月10日 16:56
+ */
+
+@Component
+@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+public class DirectReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+
+}
